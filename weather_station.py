ffc00268361c14c47121c1e1130d19e7a8ecd305
Fernando Garcia <fernando.garcia.f2@gmail.com>
2018-03-14 09:29:30 -0700

    print pressure

--- weather_station.py
+++ weather_station.py
@@ -81,6 +81,7 @@ def main():
             UV = ir_uv_sensor.readUV()
             uvIndex = UV / 100.0
 
+            print(pressure)
             setText('Temp: {:5.3f} Hum: {:5.3f} Pres: {:5.3f}, Air: {:5.3f}, Visible: {:5.3f}, IR: {:5.3f}, UV: {:5.3f}, UV Index: {:5.3f}'.format(temp_f, humidity, pressure, air_sensor_value, readVisible, IR, UV, uvIndex))
 
             # get the current minute
